e5b0ebbc7976c794094855f6643184f267f09f0d
skullY <skullydazed@gmail.com>
2017-04-06 15:19:49 -0700

    Do something with the data

--- src/qmk_bot.py
+++ src/qmk_bot.py
@@ -1,7 +1,10 @@
+from time import time
+
 from github_webhook import Webhook
 from flask import Flask
 
 # Setup
+from os.path import exists
 app = Flask(__name__)
 github_secret = None
 webhook = Webhook(app, '/postreceive', github_secret)
@@ -20,4 +23,9 @@ def healthcheck():
 
 @webhook.hook('push')
 def on_push(data):
-    print("Got push with: {0}".format(data))
+    """Handle a webhook from github.
+    """
+    timestamp = time()
+    if exists('/srv/qmk_bot/received_hooks'):
+        open('/srv/qmk_bot/received_hooks/%s.repr' % timestamp, 'w').write(repr(data))
+        open('/srv/qmk_bot/received_hooks/%s.str' % timestamp, 'w').write(str(data))
